b6604268a4d7a52478c839d4ad19ffbb5386a6f1
JacksonKearl <jkearl11@gmail.com>
2015-01-31 15:35:47 -0800

    Working force movement and scale

--- Vector Adder/GameScene.swift
+++ Vector Adder/GameScene.swift
@@ -9,23 +9,86 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var validTouchActive = false
+    var nodeToMove:VAArrow?
+    var scale:CGFloat = 1.0 {
+        didSet {
+            for child in mainNode!.children {
+                if child is VAArrow {
+                    (child as VAArrow).reScale(scale)
+                }
+            }
+        }
+    }
+    var mainNode:SKShapeNode?
+    
     override func didMoveToView(view: SKView) {
         let squareFrame = CGRectMake(-self.frame.width/2.0, -self.frame.width/2.0, self.frame.width, self.frame.width)
-        let mainNode = SKShapeNode(rect: squareFrame)
-        mainNode.fillColor = .blueColor()
-        mainNode.strokeColor = .clearColor()
-        mainNode.position = CGPointMake(self.frame.width/2 , self.frame.height-self.frame.width/2)
-        self.addChild(mainNode)
+        mainNode = SKShapeNode(rect: squareFrame)
+        mainNode!.fillColor = .blueColor()
+        mainNode!.strokeColor = .clearColor()
+        mainNode!.position = CGPointMake(self.frame.width/2 , self.frame.height-self.frame.width/2)
+        mainNode!.name = "Simulation Background"
+        self.addChild(mainNode!)
         
-        let initialArrow = VAArrow()
-        initialArrow.anchorPoint = CGPointMake(4/33.0, 12/24.0)
-
-        mainNode.addChild(initialArrow)
+        let initialArrow = VAArrow(color: UIColor.blackColor(), magnitude: 2, scale: scale)
+        
+        let centerDot = SKShapeNode(circleOfRadius: 3)
+        centerDot.fillColor = UIColor.blackColor()
+        centerDot.strokeColor = UIColor.blackColor()
+        
+        mainNode!.addChild(initialArrow)
+        mainNode!.addChild(centerDot)
+        
+        let zoomIn = SKShapeNode(circleOfRadius: 10)
+        let plusLabel = SKLabelNode(text: "+")
+        plusLabel.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
+        plusLabel.name = "Zoom In"
+        zoomIn.addChild(plusLabel)
+        zoomIn.position = CGPointMake(self.size.width/2-15, -(self.size.width/2-15))
+        mainNode!.addChild(zoomIn)
+        zoomIn.name = "Zoom In"
+        
+        let zoomOut = SKShapeNode(circleOfRadius: 10)
+        let minusLabel = SKLabelNode(text: "⎯") //This looks like a hyphen. It's not.
+        minusLabel.fontSize = 12
+        minusLabel.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
+        zoomOut.addChild(minusLabel)
+        zoomOut.position = CGPointMake(-(self.size.width/2-15), -(self.size.width/2-15))
+        mainNode!.addChild(zoomOut)
+        zoomOut.name = "Zoom Out"
+        minusLabel.name = "Zoom Out"
         
         
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-
+        let nodeTouched = self.nodeAtPoint((touches.anyObject() as UITouch).locationInNode(self))
+        
+        if let name = nodeTouched.name? {
+            switch (name) {
+            case "Arrow Handle":
+                validTouchActive = true
+                nodeToMove = (nodeTouched.parent as VAArrow)
+                
+            case "Zoom In" :
+                scale *= 2
+                
+            case "Zoom Out" :
+                scale /= 2
+            default:
+                println(nodeTouched)
+            }
+        }
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        if validTouchActive {
+            nodeToMove!.setEndPosition(touches.anyObject()!.locationInNode(mainNode), scale: scale)
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        validTouchActive = false
     }
 }
--- Vector Adder/VAArrow.swift
+++ Vector Adder/VAArrow.swift
@@ -10,11 +10,76 @@ import UIKit
 import SpriteKit
 
 class VAArrow: SKSpriteNode {
-    override init() {
-        super.init(texture: SKTexture(imageNamed: "Arrow"), color: UIColor.blackColor(), size: CGSizeMake(33,24))
+    var magnitude:CGFloat = 0
+    
+    private var magnitudeLabel = SKLabelNode(fontNamed: "Courier")
+    private var touchArea = SKShapeNode(circleOfRadius: 10)
+    private var previousScale:CGFloat = 1.0
+    private var endPoint = CGPointMake(66, 0)
+    
+    private var length: Double = 33.0
+    
+    init(color: UIColor, magnitude mag: CGFloat, scale: CGFloat) {
+        super.init(texture: SKTexture(imageNamed: "Arrow"), color: color, size: CGSizeMake(33,24))
+        self.colorBlendFactor = 1
         self.centerRect = CGRectMake(5/33.0, 12/24.0, 2/33.0, 3/24.0)
-        self.xScale = 4;
+        self.magnitude = mag
+        self.anchorPoint = CGPointMake(0/33.0, 12/24.0)
+
+        
+        touchArea.position = CGPointMake(33-10/(magnitude * scale), 0)
+        touchArea.name = "Arrow Handle"
+        touchArea.xScale = 1/(magnitude*scale);
+        touchArea.lineWidth = 0;
+        
+        magnitudeLabel.text = NSString(format: "%.2f", Double(magnitude))
+        magnitudeLabel.position = CGPointMake((33-10)/2, 0)
+        magnitudeLabel.xScale = 1/(magnitude*scale)
+        magnitudeLabel.fontSize = 10
+        magnitudeLabel.zPosition = 2
+        addChild(magnitudeLabel)
+        
+        self.addChild(touchArea)
+            
+        self.xScale = magnitude*scale;
+        previousScale = scale
     }
+    
+    func setEndPosition(e:CGPoint, scale:CGFloat){
+        
+        endPoint = e
+        previousScale = scale
+        setLength(magnitude * scale)
+
+        var angle:CGFloat = atan(e.y/e.x)
+        angle = (e.x < 0) ? angle+3.141592 : angle
+        self.zRotation = angle
+        let distToEnd = sqrt(e.x*e.x + e.y*e.y)
+        magnitude = distToEnd/(scale*33)
+        
+        
+        magnitudeLabel.text = NSString(format: "%.2f", Double(magnitude))
+        magnitudeLabel.zRotation = (e.x < 0) ? 3.141592 : 0
+        
+        
+
+    }
+    
+    func reScale(scale: CGFloat) {
+        let newEndPoint = CGPointMake(endPoint.x*(scale/previousScale), endPoint.y*(scale/previousScale))
+        setEndPosition(newEndPoint, scale: scale)
+    }
+    
+    func setLength (length: CGFloat) {
+        self.xScale = length
+        magnitudeLabel.xScale = 1/length
+        touchArea.xScale = 1/length
+        
+        touchArea.position = CGPointMake(33-10/length, 0)
+
+    }
+    
+    
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
